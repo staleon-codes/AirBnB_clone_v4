b6a848786ba6c14172e933ae1a636a43ff9822a6
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2024-02-11 12:29:25 +0200

    Update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
+Fabric script that makes a tgz archive from materials of the web_static
 folder of the AirBnB Clone repo
 """
 
