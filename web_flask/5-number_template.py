10845b33c3e591bd781b91555843f75e1af9b2e8
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2024-02-11 12:54:56 +0200

    Update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Starts Flash Web Application """
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Prints a Message when / is called """
+    """ Prints Message when / is called """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Prints a Message when /hbnb is called """
+    """ Prints Message when /hbnb is called """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ Prints a Message when /c is called """
+    """ Prints Message when /c is called """
     return "C " + text.replace('_', ' ')
 
 
@@ -31,7 +31,7 @@ def python_is_cool(text='is_cool'):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_n_number(n):
-    """ Prints a Message when /number is called only if n is an int"""
+    """ Prints Message when /number is called only if n is an int"""
     return "{:d} is a number".format(n)
 
 
