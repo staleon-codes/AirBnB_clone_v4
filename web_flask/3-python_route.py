bc00244155d85bec1eee89992db9110d1c376dd3
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2024-02-11 12:53:14 +0200

    Update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application Python is Cool"""
+""" Starts Flash Web Application Python is Cool"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Prints a Message when / is called """
+    """ Prints Message when / is called """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Prints a Message when /hbnb is called """
+    """ Prints Message when /hbnb is called """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ Prints a Message when /c is called """
+    """ Prints Message when /c is called """
     return "C " + text.replace('_', ' ')
 
 
